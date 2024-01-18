1b420b7178d322a719a3c5529e63898c7f9429ee
munee <121735319+alaneelee@users.noreply.github.com>
2024-01-18 10:54:23 +0900

    Non-Blocking, MultiPlexing Examples

--- src/test/java/com/example/playground/BlockNonBlockExamples.java
+++ src/test/java/com/example/playground/BlockNonBlockExamples.java
@@ -9,6 +9,8 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.SocketOption;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.time.LocalDateTime;
@@ -18,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class BlockNonBlockExamples {
+
     @Test
     public void blockExample() {
         int port = 8080;
@@ -28,7 +31,8 @@ public class BlockNonBlockExamples {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Connected Client: " + clientSocket.getInetAddress());
 
-                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
                 String inputLine = reader.readLine();
                 System.out.println("Received message: " + inputLine);
 
@@ -78,4 +82,56 @@ public class BlockNonBlockExamples {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void multiplexingExample() {
+        int BUFFER_SIZE = 1024;
+        int PORT = 9000;
+
+        try {
+            Selector selector = Selector.open();
+
+            ServerSocketChannel serverChannel = ServerSocketChannel.open();
+            serverChannel.bind(new InetSocketAddress(PORT));
+            serverChannel.configureBlocking(false);
+
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("Started server and port " + PORT);
+
+            while (true) {
+                selector.select();
+
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                for (SelectionKey key : selectedKeys) {
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                        SocketChannel clientChannel = serverSocketChannel.accept();
+                        clientChannel.configureBlocking(false);
+                        clientChannel.register(selector, SelectionKey.OP_READ);
+                        System.out.println(
+                            "Accepted new connection " + clientChannel.getRemoteAddress());
+                    } else if (key.isReadable()) {
+                        SocketChannel clientChannel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+                        int bytesRead = clientChannel.read(buffer);
+                        if (bytesRead == -1) {
+                            System.out.println(
+                                "Connection closing " + clientChannel.getRemoteAddress());
+                            clientChannel.close();
+                            key.cancel();
+                        } else if (bytesRead > 0) {
+                            buffer.flip();
+                            byte[] data = new byte[buffer.limit()];
+                            buffer.get(data);
+                            System.out.println("Received message " + new String(data));
+                        }
+                    }
+                }
+                selectedKeys.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
